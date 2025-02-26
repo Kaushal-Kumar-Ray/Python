7cb32b36dd12fecec67cd04aab41d2b09a50f07c
Kaushal-Kumar-Ray <raykaushal456@gmail.com>
2025-02-26 11:38:23 +0530

    string as array

---
+++ Chapter 3/string_as_array.py
@@ -0,0 +1,5 @@
+pie = "ApplePie"
+print(pie[:5])      #Slicing from Start
+print(pie[5:])      #Slicing till End
+print(pie[2:6])     #Slicing in between
+print(pie[-8:])     #Slicing using negative index
