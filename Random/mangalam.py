0e088eef621f8d4545351778d57f8b8039611814
Kaushal-Kumar-Ray <raykaushal456@gmail.com>
2025-03-06 22:34:33 +0530

    mangalam

---
+++ Random/mangalam.py
@@ -0,0 +1,16 @@
+# search for a number 36 in tuple using loop .
+#(1,4,9,16,25,36,49,64,81,100)
+
+nums = ( 1 ,4,9,16,25,36,49,64,81,100 , 36)
+X=36
+i=0
+while (i < len(nums)):
+     if (nums[i]==X):
+         print("found at idx ",i)
+       
+     else:
+             print("finding")
+     i+=1
+             
+
+         # check karo output nahi ayya rat ka h
