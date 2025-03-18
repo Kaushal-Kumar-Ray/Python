6edb6c7d90579b4349ebd7f1a9b5a188b25f215b
Kaushal-Kumar-Ray <raykaushal456@gmail.com>
2025-03-18 08:01:13 +0530

    Join sets

--- Chapter 5/Stes/Remove_items.py
+++ Chapter 5/Stes/Remove_items.py
@@ -17,3 +17,24 @@ cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
 #cities.remove("Seoul")
 cities.discard("Seoul")
 print(cities)
+
+
+'''
+There are various other methods to remove items from the set: pop(), del(), clear().'''
+#1 pop() method: we dont know which item will be removed.
+cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+print(cities.pop()) # item removed
+print(cities)
+
+#2 del() method: we can remove the whole set.
+cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+del cities
+#print(cities) # error
+
+#3 clear() method: we can remove all items from the set.
+cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+cities.clear()
+print(cities) # empty set
+
+
+
--- Random/mangalam.py
+++ Random/mangalam.py
@@ -1,16 +1,20 @@
-# search for a number 36 in tuple using loop .
-#(1,4,9,16,25,36,49,64,81,100)
 
+'''
 nums = ( 1 ,4,9,16,25,36,49,64,81,100 , 36)
 X=36
 i=0
-while (i < len(nums)):
-     if (nums[i]==X):
-         print("found at idx ",i)
-       
-     else:
-             print("finding")
-     i+=1
-             
+while i < len(nums):
+    if (nums[i]==X):
+        print("found at idx ",i) 
+    else:  
+        print("finding...")
+    i+=1
+         '''
+             # second program ( pass ko ham if/else ke andar bhi use kar sakte hain)
+for i in range(5):
+    if i==2:
+        pass
+        print("pass block")
+    print("do exercise regularly",i)
 
          # check karo output nahi ayya rat ka h
