3e20c615cc7ce51e7ba64c3b90fa72fadd1841ec
Kaushal-Kumar-Ray <raykaushal456@gmail.com>
2025-03-18 22:19:39 +0530

    class and object use

---
+++ oop Class practice/simple_class.py
@@ -0,0 +1,10 @@
+class Human_body:
+    eye=2
+    leg=2
+    def speak(self):
+        print("Hello I am Human")
+        
+person=Human_body() 
+print(person.eye)
+print(person.leg)
+person.speak()       
